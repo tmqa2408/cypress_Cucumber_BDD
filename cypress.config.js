c65847640e1f4fe7cf22a624a424781aed9f626a
tmqa2408 <tmqa2408@gmail.com>
2025-04-29 14:39:43 -0600

    delited row in a config file

--- cypress.config.js
+++ cypress.config.js
@@ -10,7 +10,6 @@ module.exports = defineConfig({
       baseUrl:'https://ecommerce-playground.lambdatest.io/index.php?route=common/home',
       reporter: 'mochawesome',
     reporterOptions: {
-      reportDir: 'cypress/results',
       overwrite: false,
       html: false,
       json: true,
