c9397d05ea9ead1f6f68484cfe8b7f6d735f11f2
tmqa2408 <tmqa2408@gmail.com>
2025-04-29 14:35:03 -0600

    fixed typo

--- cypress.config.js
+++ cypress.config.js
@@ -2,19 +2,19 @@ const { defineConfig } = require("cypress");
 const cucumber = require("cypress-cucumber-preprocessor").default;
 module.exports = defineConfig({
   projectId: 'vgsrrw',
-  e2e: {
-     specPattern: "**/*.feature",
-    setupNodeEvents(on, config) {
-      on("file:preprocessor", cucumber());
+    e2e: {
+      specPattern: "**/*.feature",
+      setupNodeEvents(on, config) {
+        on("file:preprocessor", cucumber());
+      },
+      baseUrl:'https://ecommerce-playground.lambdatest.io/index.php?route=common/home',
+      reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: false,
+      json: true,
     },
-    baseUrl:'https://ecommerce-playground.lambdatest.io/index.php?route=common/home',
-    reporter: 'mochawesome',
-  reporterOptions: {
-    reportDir: 'cypress/results',
-    overwrite: false,
-    html: false,
-    json: true,
-  
   },
   
 
